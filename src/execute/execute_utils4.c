93d3431154d59cc74fade30af6aef33a7cb56548
Georgiy <kankiyagg@gmail.com>
2025-09-21 16:00:11 +0400

    commit

--- src/execute/execute_utils4.c
+++ src/execute/execute_utils4.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_utils4.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gkankia <gkankia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: georgy-kankiya <georgy-kankiya@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 20:28:23 by gkankia           #+#    #+#             */
-/*   Updated: 2025/09/15 20:57:44 by gkankia          ###   ########.fr       */
+/*   Updated: 2025/09/21 15:59:50 by georgy-kank      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 void	check_no_command_redirection(t_minishell *sh)
 {
 	if (!sh->args[1])
+	{
 		sh->exit_code = 2;
+		printf("minishell: syntax error near unexpected token `newline'\n");
+	}
 	else if (is_redirections(sh, 0) && is_redirections(sh, 1))
+	{
 		sh->exit_code = 2;
+		printf("minishell: syntax error near unexpected token `%s'\n", \
+sh->args[1]);
+	}
 	else if (ft_strcmp(sh->args[0], "<") == 0)
 	{
-		sh->exit_code = 1;
-		if (access(sh->args[1], F_OK) == 0)
-			sh->exit_code = 0;
+		sh->exit_code = 0;
+		if (access(sh->args[1], F_OK) != 0)
+			sh->exit_code = 1;
 	}
 	else if (ft_strcmp(sh->args[0], ">") == 0 || \
 ft_strcmp(sh->args[0], ">>") == 0)
